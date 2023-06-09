4f31134b38e6beeaa9d7370cf77299a194d8b055
Kayo Leanndro <kayoleanndro2@gmail.com>
2023-06-09 15:40:12 -0300

    update archives 09/06 - 15:40

--- src/main/java/br/com/project/api/controle/Controle.java
+++ src/main/java/br/com/project/api/controle/Controle.java
@@ -2,6 +2,9 @@ package br.com.project.api.controle;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,37 +33,59 @@ public class Controle {
     }
 
     @GetMapping("/api/{codigo}")
-    public Pessoa selecionarPeloCodigo(@PathVariable int codigo){
+    public Pessoa selecionarPeloCodigo(@PathVariable int codigo) {
         return acao.findByCodigo(codigo);
     }
+
     @PutMapping("/api")
-    public Pessoa editar(@RequestBody Pessoa obj){
+    public Pessoa editar(@RequestBody Pessoa obj) {
         return acao.save(obj);
     }
 
     @DeleteMapping("/api/{codigo}")
-    public void remover(@PathVariable int codigo){
-       Pessoa obj = selecionarPeloCodigo(codigo);
+    public void remover(@PathVariable int codigo) {
+        Pessoa obj = selecionarPeloCodigo(codigo);
 
-       acao.delete(obj);
+        acao.delete(obj);
     }
-    
+
     @GetMapping("/api/contador")
-    public long contador(){
+    public long contador() {
         return acao.count();
     }
 
     @GetMapping("/api/ordenarNomes")
-   public List<Pessoa> ordenarNomes(){
-    return acao.findByOrderByNome();
-   }
+    public List<Pessoa> ordenarNomes() {
+        return acao.findByOrderByNome();
+    }
 
-    
-    
+    @GetMapping("/api/ordenarNomes2")
+    public List<Pessoa> ordenarNomes2() {
+        return acao.findByNomeOrderByIdadeDesc("Kayo");
+    }
 
+    @GetMapping("/api/iniciaCom")
+    public List<Pessoa> iniciaCom() {
+        return acao.findByNomeStartingWith("K");
+    }
 
+    @GetMapping("/api/terminaCom")
+    public List<Pessoa> terminaCom() {
+        return acao.findByNomeEndsWith("a");
+    }
+    @GetMapping("/api/somaIdades")
+    public int somaIdades(){
+        return acao.somaIdades();
+    }
+    @GetMapping("/api/idadeMaiorIgual")
+    public List<Pessoa> idadeMaiorIgual(){
+        return acao.idadeMaiorIgual(18);
+    }
 
-
+    @GetMapping("/status")
+    public ResponseEntity<?> status(){
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 
 
 
